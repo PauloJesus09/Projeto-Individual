b8c94d386be382fbbbc86cf1f2c9fac47ef142e7
PauloJesus09 <paulo.jesus@sptech.school>
2025-11-13 14:27:44 -0300

    Implementando mais validações

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -22,19 +22,52 @@ function aguardar() {
     var divAguardar = document.getElementById("div_aguardar");
     divAguardar.style.display = "flex";
 
-    setTimeout(() => {
-        window.location = "login.html";
-    }, 2000);
+    if (finalizarAguardar == true) {
+        setTimeout(() => {
+            window.location = "login.html";
+        }, 2000);
+    } else {
+        setTimeout(() => {
+            window.location = "cadastro.html";
+        });
+    }
 }
 
-function finalizarAguardar(texto) {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "none";
+function finalizarAguardar() {
 
-    var divErrosLogin = document.getElementById("div_erros_login");
-    if (texto) {
-        divErrosLogin.style.display = "flex";
-        divErrosLogin.innerHTML = texto;
+    var nomeVar = ipt_nome.value;
+    var emailVar = ipt_email.value;
+    var senhaVar = ipt_senha.value;
+    var mensagem = "";
+    if (nomeVar == '' || emailVar == '' || senhaVar == '') {
+      ipt_nome.style.border = "1px solid red";
+      ipt_email.style.border = "1px solid red";
+      ipt_senha.style.border = "1px solid red";
+      mensagem += `Preencha todos os campos!`;
     }
+
+    if (emailVar.endsWith('@gmail.com') || emailVar.endsWith('@hotmail.com') || emailVar.endsWith('@outlook.com') || emailVar.endsWith('@yahoo.com') || emailVar.endsWith('@sptech.school') || emailVar.endsWith('@email.com')) {
+      // o email precisa terminar com umas dessas validaçôes.
+      ipt_email.style.border = "1px solid green";
+    } else {
+
+      ipt_email.style.border = "1px solid red";
+      msgEmail.innerHTML = `Insira um email válido!`
+    }
+
+    if (senhaVar < 6) {
+      ipt_senha.style.border = "1px solid red";
+      msgSenha.innerHTML = `Senha precisa conter no mínimo 6 caracteres!`
+    }
+
+    div_msg.innerHTML = mensagem;
+    // var divAguardar = document.getElementById("div_aguardar");
+    // divAguardar.style.display = "none";
+    
+    // var divErrosLogin = document.getElementById("div_erros_login");
+    // if (texto) {
+    //     divErrosLogin.style.display = "flex";
+    //     divErrosLogin.innerHTML = texto;
+    // }
 }
 
