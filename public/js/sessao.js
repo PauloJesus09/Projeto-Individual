c2c17495f43061570bd58926ce2b33144bc9478e
PauloJesus09 <paulo.jesus@sptech.school>
2025-11-13 22:34:56 -0300

    Validações finalizadas na tela de Login e Cadastro!

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -17,50 +17,21 @@ function limparSessao() {
     window.location = "login.html";
 }
 
-// carregamento (loading)
-function aguardar() {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "flex";
-
-    if (finalizarAguardar == true) {
-        setTimeout(() => {
-            window.location = "login.html";
-        }, 2000);
-    } else {
-        setTimeout(() => {
-            window.location = "cadastro.html";
-        });
+function limparErro(campoInput) {
+    if (campoInput == 'nome') {
+        msgNome.innerHTML = "";
+        ipt_nome.style.border = `1px solid #ccc`;
+    } 
+    if (campoInput == 'email') {
+        msgEmail.innerHTML = "";
+        ipt_email.style.border = `1px solid #ccc`;
+    } 
+    if (campoInput == 'senha') {
+        msgSenha.innerHTML = "";
+        ipt_senha.style.border = `1px solid #ccc`;
     }
 }
-
-function finalizarAguardar() {
-
-    var nomeVar = ipt_nome.value;
-    var emailVar = ipt_email.value;
-    var senhaVar = ipt_senha.value;
-    var mensagem = "";
-    if (nomeVar == '' || emailVar == '' || senhaVar == '') {
-      ipt_nome.style.border = "1px solid red";
-      ipt_email.style.border = "1px solid red";
-      ipt_senha.style.border = "1px solid red";
-      mensagem += `Preencha todos os campos!`;
-    }
-
-    if (emailVar.endsWith('@gmail.com') || emailVar.endsWith('@hotmail.com') || emailVar.endsWith('@outlook.com') || emailVar.endsWith('@yahoo.com') || emailVar.endsWith('@sptech.school') || emailVar.endsWith('@email.com')) {
-      // o email precisa terminar com umas dessas validaçôes.
-      ipt_email.style.border = "1px solid green";
-    } else {
-
-      ipt_email.style.border = "1px solid red";
-      msgEmail.innerHTML = `Insira um email válido!`
-    }
-
-    if (senhaVar < 6) {
-      ipt_senha.style.border = "1px solid red";
-      msgSenha.innerHTML = `Senha precisa conter no mínimo 6 caracteres!`
-    }
-
-    div_msg.innerHTML = mensagem;
+function finalizarAguardar(texto) {
     // var divAguardar = document.getElementById("div_aguardar");
     // divAguardar.style.display = "none";
     
@@ -71,3 +42,16 @@ function finalizarAguardar() {
     // }
 }
 
+// carregamento (loading)
+function aguardar() {
+    var divAguardar = document.getElementById("div_aguardar");
+    divAguardar.style.display = "none";
+
+    // if (finalizarAguardar == true) {
+    //     setTimeout(() => {
+    //         window.location = "login.html";
+    //     }, 2000);
+    // } else {
+    //     // div_msg.innerHTML = `Campos inválidos`;
+    // } 
+}
